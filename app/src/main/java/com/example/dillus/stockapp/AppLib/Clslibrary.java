57181ee2c998646f9cc70b3d9a70dbfc93c6ecf0
DillusLab <dillus.lab.bo@gmail.com>
2023-01-06 14:48:53 -0400

    commit-06012023-001

---
+++ app/src/main/java/com/example/dillus/stockapp/AppLib/Clslibrary.java
@@ -0,0 +1,25 @@
+package com.example.dillus.stockapp.AppLib;
+
+public class Clslibrary {
+
+    // REQUEST CODE
+    public static final int REQUEST_CODE_SIGN_IN = 10001;
+
+    // DATABASE FIREBASE
+    public static final String COLLECTION_USERS = "users";
+    public static final String COLLECTION_CATEGORIA = "categoria";
+    public static final String COLLECTION_TIENDA = "tienda";
+
+    // CAMPO ATRIBUTOS
+    public static final String FIREBASE_ID = "id";
+    public static final String FIREBASE_NAME = "name";
+    public static final String FIREBASE_EMAIL = "email";
+    public static final String FIREBASE_PASSWORD = "password";
+
+
+    // COUNT DOWN
+    public static final String STRING_COUNTDOWN = "STRING_COUNTDOWN";
+    public static final long COUNTDOWN_0 = 0;
+    public static final long COUNTDOWN = 3000;
+
+}
