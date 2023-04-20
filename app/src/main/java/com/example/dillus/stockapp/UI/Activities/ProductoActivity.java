405ab26ebcd25046b7d05e413487362b3948b11e
dillus <dillus.lab.bo@gmail.com>
2023-04-20 16:45:04 -0400

    20-04-2023

--- app/src/main/java/com/example/dillus/stockapp/UI/Activities/ProductoActivity.java
+++ app/src/main/java/com/example/dillus/stockapp/UI/Activities/ProductoActivity.java
@@ -50,6 +50,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
